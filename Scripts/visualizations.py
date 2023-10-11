feb82d918caf17341a674123e9f14bcf97047e6d
Vitto R <kinzer@berkeley.edu>
2023-10-11 09:48:44 -0700

    added singplot function

--- Scripts/visualizations.py
+++ Scripts/visualizations.py
@@ -337,3 +337,108 @@ def compplot(sound1,sampling_rate1,
         if play_sounds:
             pulse1(sound1,sampling_rate)
             pulse1(sound2,sampling_rate)
+            
+            
+def singplot(sound1,sampling_rate1,
+             vmin1=-100,
+             logfs=False,logfp=True,logPb=True,
+            s1frange=False,s2frange=False):
+
+        fig, axs = plt.subplots(2,2,sharex=False,figsize=(9,10),#constrained_layout=True,
+                                gridspec_kw={'height_ratios': [1,2],'width_ratios': [1,2]})
+        fig.delaxes(axs[0,0])
+        k1 = sound1.shape[0]
+        x1=(np.arange(k1))/sampling_rate1
+        axs[0,1].plot(x1,sound1)
+
+        axs[0,1].set_ylabel("Amplitude (AU)") #Units: arbitrary units or decibels
+        axs[0,1].set_xlim([0,k1/sampling_rate1])
+
+        axs[1,1].specgram(sound1,Fs=sampling_rate1,cmap='jet',vmin=vmin1)
+        axs[1,1].set_xlabel('Time (s)')
+        axs[1,1].set_ylabel("Freq (kHz)")
+        scale = 1e3 #Hz=1kHz
+        ticks = matplotlib.ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale))
+        axs[1,1].yaxis.set_major_formatter(ticks)
+        if logfs == True: 
+            axs[1,0].set_yscale("log")
+            axs[1,1].set_yscale("log")
+        if s1frange != False:
+            axs[1,0].set_ylim(s1frange)
+        if s2frange != False:
+            axs[1,1].set_ylim(s2frange)
+        
+        
+        sound = sound1
+        fs = sampling_rate1
+        f,P = periodogram(sound,fs=fs) #extract frequency & PSD 
+
+        #Calculated Cum. Integral
+        P_cumtrapz = integrate.cumtrapz(P, f, initial=0) #calculate cumultative integral (of PSD)
+        P_cumtrapz = P_cumtrapz/np.max(P_cumtrapz)
+
+        if logPb==True:
+            logP = 10*np.log10(np.abs(P)/np.max(np.max(np.abs(P))))
+            logP_cumtrapz = integrate.cumtrapz(logP-np.min(logP[1:-1]), f, initial=0) #calculate cumultative integral (of PSD)
+            logP_cumtrapz = logP_cumtrapz/np.max(logP_cumtrapz)
+
+        #Plotting
+        if logPb==True:
+
+            lns1 = axs[1,0].plot(logP[1:], f[1:], 'b',label='PSD',linewidth = 0.25)
+            ax4 = axs[1,0].twiny()
+
+            lns2 = ax4.plot(logP_cumtrapz,f,'r-',label='Cum. Integral')
+            lns3 = ax4.plot([0,np.max(P_cumtrapz)],[ultrasound_threshold,ultrasound_threshold],'g',
+                        label = 'Threshold = '+str(ultrasound_threshold/1000)+' kHz')
+
+            axs[1,0].set_xlabel(r"PSD (dB), fs = "+str(fs/1000)+" kHz\n", color='b')
+            axs[1,0].invert_xaxis()
+            ax4.invert_xaxis()
+            axs[1,0].set_ylim([0,f[-1]])
+            
+
+            below = logP_cumtrapz[np.max(np.where(f <= ultrasound_threshold))]
+            above = 1 - below
+        else: 
+            lns1 = axs[1,0].plot(P, f, 'b',label='PSD',linewidth = 0.25)
+            ax4 = axs[1,0].twinx()
+
+            lns2 = ax4.plot(P_cumtrapz,f,'r-',label='Cum. Integral')
+            lns3 = ax4.plot([0,np.max(P_cumtrapz)],[ultrasound_threshold,ultrasound_threshold],'g',
+                        label = 'Threshold = '+str(ultrasound_threshold/1000)+' kHz')
+
+            axs[1,0].set_xlabel(r"PSD (V$^2$/Hz), fs = "+str(fs/1000)+" kHz\n", color='b')
+
+            below = P_cumtrapz[np.max(np.where(f <= ultrasound_threshold))]
+            above = 1 - below
+
+        if logfp==True:
+            axs[1,0].set_yscale("log")
+            ax4.set_yscale("log")
+
+        #ax4.set_title('PSD Plot, fs = '+str(fs/1000)+' kHz\n'+str(round(below*100))+'% below, '+str(round(above*100))+'% above threshold')
+        ax4.set_xlabel('Relative Cum.Int.of PSD (%)', color='r')
+
+        #Legend
+        lns = lns1+lns2+lns3
+        labs = [l.get_label() for l in lns]
+        ax4.legend(lns, labs, facecolor='white', framealpha = 1,
+                   loc='upper left', borderaxespad=0.)
+        
+        
+        all_axes = fig.get_axes()
+        for axis in all_axes:
+            legend = axis.get_legend()
+            if legend is not None:
+                legend.remove()
+                all_axes[-1].add_artist(legend)
+        #if sound2 == 0:
+         #   axs[0,1].remove()
+         #   axs[1,1].remove()
+         #   axs[2,1].remove()
+         #   ax5.remove()
+        plt.subplots_adjust(wspace=0, hspace=0)
+        plt.show()
+        if play_sounds:
+            pulse1(sound1,sampling_rate)
